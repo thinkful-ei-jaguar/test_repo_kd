0fc40ce201d44d6284d4adee5c42cc546269708e
Jason Moffit <j.moffit@hotmail.com>
2019-12-02 16:13:11 -0600

    js  not working

--- drill1.js
+++ drill1.js
@@ -1,5 +1,28 @@
-function myInfo (){
-    console.log('Hi, my name is Kris and Im 26 years old.');
+const log = console.log;
 
+function myInfo() {
+  console.log("Hi, my name is Kris and Im 26 years old.");
 }
 myInfo();
+
+// const createGreeting = () => {
+//   let greeting;
+//   function greet() {
+//     greeting = "Hello, My name is Jason";
+//     return greeting;
+//   };
+// };
+
+// // createGreeting();
+
+// const greeted = greet();
+
+// log(greet);
+
+
+function test(name, age)(
+    this.name = name;
+    this.age = age;
+)
+
+log(test('jason', 20))
