ca48a1973058b5a2049c5653ee6bf0803ffcc7a9
Jason Moffit <j.moffit@hotmail.com>
2019-12-02 21:10:28 -0600

    js working with an added touch from menteor session

--- drill1.js
+++ drill1.js
@@ -1,9 +1,9 @@
-const log = console.log;
+// const log = console.log;
 
-function myInfo() {
-  console.log("Hi, my name is Kris and Im 26 years old.");
-}
-myInfo();
+// function myInfo() {
+//   console.log("Hi, my name is Kris and Im 26 years old.");
+// }
+// myInfo();
 
 // const createGreeting = () => {
 //   let greeting;
@@ -18,11 +18,20 @@ myInfo();
 // const greeted = greet();
 
 // log(greet);
+const log = console.log;
 
+const makeGreeting = function() {
+  let greet;
+  return function(name, age) {
+    greet = `My name is ${name} and I am ${age()}`;
+    return greet;
+  };
+};
+const greetings = makeGreeting();
 
-function test(name, age)(
-    this.name = name;
-    this.age = age;
-)
-
-log(test('jason', 20))
+// log(greetings("Jason", 40));
+log(
+  greetings("Jason", function() {
+    return 40;
+  })
+);
